d1ba41042ddc8f0410c4cc04b62b09cef5b4c4c3
BhaveshJibhakate <130980313+BhaveshJibhakate@users.noreply.github.com>
2023-07-10 07:58:19 +0530

    Add files via upload

---
+++ FilmStudio/src/main/java/com/app/services/ProducerServiceImpl.java
@@ -0,0 +1,13 @@
+package com.app.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.Getter;
+
+@Service
+@Transactional
+
+public class ProducerServiceImpl implements ProducerServiceIF {
+
+}
